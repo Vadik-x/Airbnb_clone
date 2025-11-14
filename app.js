25ab7448c91f83cfaa3ee835cce2960a18ec7260
Vadik Malik <vadik.core@gmail.com>
2025-11-14 21:04:13 +0530

    fixed port bug

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ async function main() {
 }
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
